0c39790f9256816c656defa8d1919975d46455f0
rolandnii <rolandniidodoo@gmail.com>
2023-11-19 17:29:56 +0000

    initial

---
+++ Logger.cs
@@ -0,0 +1,24 @@
+﻿
+namespace Logging
+{
+	  class Logger
+	{
+		public LogLevel EnabledLevel { get; init; } = LogLevel.Error;
+
+		public void LogMessage(LogLevel level, string msg)
+		{
+			if (EnabledLevel < level) return;
+            Console.WriteLine(msg);
+        }
+	}
+
+	 enum LogLevel 
+	{
+		Off, 
+		Critical,
+		Error,
+		Warning,
+		Information,
+		Trace,
+	}
+}
